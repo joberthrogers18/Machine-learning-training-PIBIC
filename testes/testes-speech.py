2c42d6806d898974e7367a5324c7b2109b071db7
Joberth Rogers <joberth.rogers18@gmail.com>
2019-07-13 21:49:47 -0400

    Made some fixes minor

--- testes/testes-speech.py
+++ testes/testes-speech.py
@@ -5,6 +5,9 @@ r = sr.Recognizer()
 acting_so_weird = sr.AudioFile ('you-are-acting-so-weird.wav')
 
 with acting_so_weird as source:
+    # tentando tirar os ruidos do baralho
+    r.adjust_for_ambient_noise(source)
     audio = r.record(source)
 
-print(r.recognize_google(audio))
+# show: True, retorna um JSON da api com todos os possiveis resultados
+print(r.recognize_google(audio, show_all=True))
