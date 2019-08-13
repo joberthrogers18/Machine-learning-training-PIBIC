68b17b5fe9b750b8442e638bf2dbb276730eb65c
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-13 20:05:33 -0300

    get the arguments and search for offset volume userdata

--- try_speech/script/script.py
+++ try_speech/script/script.py
@@ -2,26 +2,41 @@ import os
 import argparse
 
 
+def remove_whitespace(current_list):
+    while '' in current_list:
+        current_list.remove('')
+
+    return current_list
+
+
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-f", 
+        "-f",
         help="name of file that will be collected",
         required=True
     )
     parser.add_argument(
-        "-d", 
-        help="Destination of file", 
+        "-d",
+        help="Destination of file",
         required=True
     )
+
     args = parser.parse_args()
-    print(args.d, args.f)
+    mmls = os.popen('mmls ' + args.f).read()
+
+    split_mmls = mmls.split('\n')
+
+    for word in split_mmls:
+        if "userdata" in word:
+            offset_mmls = word.split(" ")
+            print(offset_mmls)
+            break
+
+    offset_mmls = remove_whitespace(offset_mmls)
     
 
-# version = os.system("mmls -V")
-
-# print(version)
 
 if __name__ == "__main__":
     main()
