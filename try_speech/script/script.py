0ba79022559a8f2e71ad761040171a0b54532c3c
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-13 22:04:05 -0300

    Found the inode

--- try_speech/script/script.py
+++ try_speech/script/script.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import re
 
 
 def remove_whitespace(current_list):
@@ -9,6 +10,23 @@ def remove_whitespace(current_list):
     return current_list
 
 
+def find_directory(file, offset, searched_directory):
+
+    inode = ''
+
+    fls = os.popen('fls -o ' + offset + ' ' + file + ' ' + inode).read()
+    fls = fls.replace('\t', ' ').replace(':', '')
+    split_fls = fls.split('\n')
+    split_fls = remove_whitespace(split_fls)
+
+    for directorie in split_fls:
+        if re.search(r'\b' + searched_directory + r'\b', directorie):
+            next_directorie = directorie.split(' ')
+            inode = next_directorie[1]
+
+    return inode
+
+    
 def main():
     parser = argparse.ArgumentParser()
 
@@ -31,12 +49,26 @@ def main():
     for word in split_mmls:
         if "userdata" in word:
             offset_mmls = word.split(" ")
-            print(offset_mmls)
             break
 
     offset_mmls = remove_whitespace(offset_mmls)
     
+    list_directorie = ['media', '0', 'WhatsApp', 'Media', 'WhatsApp Voice Notes']
 
+    inode = ''
+    
+    inode = find_directory(args.f, offset_mmls[-4], list_directorie[0])
+    print(inode)
+    '''  
+    cont = 0
+    while(list_directorie != [] or cont < 20):
+        
+        for directorie in list_directorie:
+            for current_dir in split_fls:
+
+                if directorie in current_dir:
+                    print(current_dir)
+        cont += 1'''
 
 if __name__ == "__main__":
     main()
