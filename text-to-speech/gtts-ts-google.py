f6c67d5292e3bf0b6a56bbbd4602b5b78a16d7d9
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-24 17:49:19 -0300

    Manipulating the script to take all files txt
    In current directorie and read it content inside

--- text-to-speech/gtts-ts-google.py
+++ text-to-speech/gtts-ts-google.py
@@ -1,12 +1,41 @@
-
 import os 
 from gtts import gTTS
 
-Text = "Meu parcerinho demais cara"
+def filter_dirs(dirs):
 
-print("Espere... processando")
-TTS = gTTS(text=Text, lang='pt-br')
+  exclude = ['bin', 'include', 'lib', '.vscode']
 
-TTS.save("voice.mp3")
+  dirs[:] = [d for d in dirs if d not in exclude]
+  return dirs
 
-os.system("start voice.mp3")
+def find_txts():
+
+  files_txt = []
+
+  for root, dirs, files in os.walk(os.getcwd(), topdown=True):   
+    dirs = filter_dirs(dirs)
+
+    for file in files:
+      if file.endswith('.txt'):
+        print('passou')
+        files_txt.append(os.path.join(root, file))
+
+  return files_txt
+
+if __name__ == '__main__':
+
+  try:
+    all_txts = find_txts()
+
+    for filename in all_txts:
+      file = open(filename, 'r')
+      content_txt = file.read()
+      print(content_txt)
+
+    print("Espere... processando")
+    TTS = gTTS(text='text', lang='pt-br')
+
+    TTS.save("voice.mp3")
+
+  except AssertionError:
+    print('Não foi possível criar audios do texto atual!!')
