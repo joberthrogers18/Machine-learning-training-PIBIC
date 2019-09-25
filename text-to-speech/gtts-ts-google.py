9f2dad564a08d1871a4f0658f7ec6e1fb2de3262
Joberth Rogers <joberth.rogers18@gmail.com>
2019-09-24 21:31:20 -0300

    Adding the files split in dataset folder generated

--- text-to-speech/gtts-ts-google.py
+++ text-to-speech/gtts-ts-google.py
@@ -22,20 +22,47 @@ def find_txts():
 
   return files_txt
 
+def treat_content_file (content_file):
+
+  list_characters = ["!", ".", "'", "/", "|", "~", "`", 
+  "@","#", "£", "$", "%", "^", "&", "*", "(", ")"]
+
+  string_split = []
+
+  for i in list_characters:
+    content_file = content_file.replace(i, " ")
+
+  string_split = content_file.split(" ")
+
+  return string_split
+
 if __name__ == '__main__':
 
   try:
     all_txts = find_txts()
 
+    content_split = []
+
+    try:
+      os.mkdir(os.getcwd() + "/dataset")
+    except (FileExistsError, OSError):
+          pass
+
     for filename in all_txts:
+      
       file = open(filename, 'r')
       content_txt = file.read()
-      print(content_txt)
+      content_split = treat_content_file(content_txt)
 
-    print("Espere... processando")
-    TTS = gTTS(text='text', lang='pt-br')
+      for word in content_split:
+        
+        try:
+          os.mkdir(os.getcwd() + "/dataset/" + word)
+        except (FileExistsError, OSError):
+          pass
 
-    TTS.save("voice.mp3")
+        TTS = gTTS(text= word, lang='pt-br')
+        TTS.save(f"dataset/{word}/{word}.mp3")
 
   except AssertionError:
     print('Não foi possível criar audios do texto atual!!')
