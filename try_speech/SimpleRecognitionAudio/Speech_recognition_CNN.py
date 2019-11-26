ce8db19e298f652025ed372c1187935d39d5c095
Joberth Rogers <joberth.rogers18@gmail.com>
2019-11-26 19:32:34 -0400

    Modularized the code of classification speech
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- try_speech/SimpleRecognitionAudio/Speech_recognition_CNN.py
+++ try_speech/SimpleRecognitionAudio/Speech_recognition_CNN.py
@@ -12,9 +12,21 @@ from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 import numpy as np
 
+# Feature dimension
+FEATURE_DIM_1 = 20
+CHANNEL = 1
+EPOCHS = 200
+BATCH_SIZE = 100
+VERBOSE = 1
+NUM_CLASSES = 8
+
+# Second dimension of the feature is dim2
+FEATURE_DIM_2 = 11
+DATASET_SIZE_MODEL = -1
+
 def get_model():
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel)))
+    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(FEATURE_DIM_1, FEATURE_DIM_2, CHANNEL)))
     model.add(Conv2D(48, kernel_size=(2, 2), activation='relu'))
     model.add(Conv2D(120, kernel_size=(2, 2), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -24,7 +36,7 @@ def get_model():
     model.add(Dropout(0.25))
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.4))
-    model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer=keras.optimizers.Adadelta(),
                   metrics=['accuracy'])
@@ -33,7 +45,7 @@ def get_model():
 # Predicts one sample
 def predict(filepath, model):
     sample = wav2mfcc(filepath)
-    sample_reshaped = sample.reshape(1, feature_dim_1, feature_dim_2, channel)
+    sample_reshaped = sample.reshape(1, FEATURE_DIM_1, FEATURE_DIM_2, CHANNEL)
     return get_labels()[0][
             np.argmax(model.predict(sample_reshaped))
     ]
@@ -41,57 +53,52 @@ def predict(filepath, model):
 #     return np.argmax(model.predict(sample_reshaped))
 
 
+def training_model():
 
-# Second dimension of the feature is dim2
-feature_dim_2 = 11
-dataset_size_model = -1
+    # Save data to array file first
+    # save_data_to_array(max_len=FEATURE_DIM_2, size_dataset=DATASET_SIZE_MODEL)
 
-# Save data to array file first
-# save_data_to_array(max_len=feature_dim_2, size_dataset=dataset_size_model)
+    # Loading train set and test set
+    X_train, X_test, y_train, y_test = get_train_test()
 
-# Loading train set and test set
-X_train, X_test, y_train, y_test = get_train_test()
+    # Reshaping to perform 2D convolution
+    X_train = X_train.reshape(X_train.shape[0], FEATURE_DIM_1, FEATURE_DIM_2, CHANNEL)
+    X_test = X_test.reshape(X_test.shape[0], FEATURE_DIM_1, FEATURE_DIM_2, CHANNEL)
 
-# # Feature dimension
-feature_dim_1 = 20
-channel = 1
-epochs = 200
-batch_size = 100
-verbose = 1
-num_classes = 8
+    y_train_hot = to_categorical(y_train)
+    y_test_hot = to_categorical(y_test)
 
-# Reshaping to perform 2D convolution
-X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
-X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+    model = get_model()
+    model.fit(X_train, y_train_hot, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=VERBOSE, validation_data=(X_test, y_test_hot))
 
-y_train_hot = to_categorical(y_train)
-y_test_hot = to_categorical(y_test)
+    # prediction = predict('./teste_audio/maconha-treino0d3a1dfab0de439ba80162ef1fa46816.wav', model=model)
+    # print(prediction)
 
-model = get_model()
-model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot))
+    return model
 
-prediction = predict('./teste_audio/maconha-treino0d3a1dfab0de439ba80162ef1fa46816.wav', model=model)
+if __name__ == '__main__':
 
-print(prediction)
+    predictions = []
+    pediction_audios = []
 
-predictions = []
-pediction_audios = []
+    # return the model trained
+    model = training_model()
 
-for file_audio in os.listdir('./teste_audio/'):
-    # Verify through the split audio if audio is suspect or not
- 
-    os.system("cp teste_audio/" + file_audio + " " + file_audio)
-    aAnaly.silenceRemovalWrapper(file_audio , smoothingWindow=1.0, weight=0.3)
+    for file_audio in os.listdir('./teste_audio/'):
+        # Verify through the split audio if audio is suspect or not
+    
+        os.system("cp teste_audio/" + file_audio + " " + file_audio)
+        aAnaly.silenceRemovalWrapper(file_audio , smoothingWindow=1.0, weight=0.3)
 
-    prediction = []
-    os.system("rm " + file_audio)
+        prediction = []
+        os.system("rm " + file_audio)
 
-    for filename in glob.glob(os.path.join(os.getcwd(), "*.wav")):
-        print(filename)
-        predictions.append( predict(filename, model=model) )
-        os.system("rm " + str(filename))
+        for filename in glob.glob(os.path.join(os.getcwd(), "*.wav")):
+            print(filename)
+            predictions.append( predict(filename, model=model) )
+            os.system("rm " + str(filename))
 
-    pediction_audios.append('Audio suspeito') if 'maconha' in predictions else pediction_audios.append('Não suspeito')
+        pediction_audios.append('Audio suspeito') if 'maconha' in predictions else pediction_audios.append('Não suspeito')
 
-print('Audios não suspeitos: ' + str( pediction_audios.count('Não suspeito') ) )
-print('Audios suspeitos: ' + str( pediction_audios.count('Audio suspeito') ) )
+    print('Audios não suspeitos: ' + str( pediction_audios.count('Não suspeito') ) )
+    print('Audios suspeitos: ' + str( pediction_audios.count('Audio suspeito') ) )
